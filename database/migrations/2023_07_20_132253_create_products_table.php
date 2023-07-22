927e8018618828167df8d3ab0f9afbc2bdbd006d
root <root@DESKTOP-01UQUJI>
2023-07-22 19:46:06 +0600

    second commit

--- database/migrations/2023_07_20_132253_create_products_table.php
+++ database/migrations/2023_07_20_132253_create_products_table.php
@@ -14,8 +14,20 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('product_title');
-            $table->string('product_code')->unique();
+            $table->char('product_code', 40)->unique();
+            $table->string('product_slug')->nullable();
+            $table->decimal('product_price', 8, 2);
+            $table->boolean('product_available')->default(true);
+            $table->integer('product_featured')->default(0);
+            $table->boolean('has_sizes')->default(true)->nullable();
+            $table->string('product_images')->nullable();
+
+            $table->unsignedInteger('product_type_id');
+
             $table->timestamps();
+        });
+        Schema::table('products', function (Blueprint $table) {
+                $table->foreign('product_type_id')->references('id')->on('product_types');
         });
     }
 
